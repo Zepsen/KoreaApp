55ffb7110d71d2a3dc6e4bf5acc5ecc1468b10b5
Dmytro Zepsen <Dmytro.Zepsen@thomsonreuters.com>
2019-10-30 12:26:04 +0200

    aadd token

---
+++ DataSource/Services/AuthService.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Handlers.Services
+{
+    public interface IAuthService
+    {
+        string GenerateToken(string email);
+
+        JwtSecurityToken Validate(string jwt);
+    }
+
+    public class AuthService : IAuthService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string email)
+        {
+            var now = DateTime.UtcNow;
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, email),                
+                new Claim(JwtRegisteredClaimNames.Iat, new DateTimeOffset(now).ToUniversalTime().ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64)
+            };
+
+            var creds = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSecurityKey"])),
+                SecurityAlgorithms.HmacSha256);
+
+            // Create the JWT and write it to a string
+            var jwt = new JwtSecurityToken(
+                issuer: _configuration["JwtIssuer"],
+                audience: _configuration["JwtIssuer"],
+                claims: claims,
+                notBefore: now,
+                expires: now.AddDays(double.Parse(_configuration["JwtExpiryInDays"])),
+                signingCredentials: creds);
+            
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+        public JwtSecurityToken Validate(string jwt)
+        {
+            SecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSecurityKey"]));
+            var validationParameters = new TokenValidationParameters
+            {
+                // Clock skew compensates for server time drift.
+                // We recommend 5 minutes or less:
+                ClockSkew = TimeSpan.FromMinutes(5),
+                // Specify the key used to sign the token:
+                IssuerSigningKey = key,
+                RequireSignedTokens = true,
+                // Ensure the token hasn't expired:
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                // Ensure the token audience matches our audience value (default true):
+                ValidateAudience = true,
+                ValidAudience = _configuration["JwtIssuer"],
+                // Ensure the token was issued by a trusted authorization server (default true):
+                ValidateIssuer = true,
+                ValidIssuer = _configuration["JwtIssuer"]
+            };
+
+            try
+            {
+                var claimsPrincipal = new JwtSecurityTokenHandler()
+                    .ValidateToken(jwt, validationParameters, out var rawValidatedToken);
+
+                return (JwtSecurityToken)rawValidatedToken;
+                // Or, you can return the ClaimsPrincipal
+                // (which has the JWT properties automatically mapped to .NET claims)
+            } catch (SecurityTokenValidationException stvex)
+            {
+                // The token failed validation!
+                // TODO: Log it or display an error.
+                throw new Exception($"Token failed validation: {stvex.Message}");
+            } catch (ArgumentException argex)
+            {
+                // The token was not well-formed or was invalid for some other reason.
+                // TODO: Log it or display an error.
+                throw new Exception($"Token was invalid: {argex.Message}");
+            }
+        }
+    }
+
+}
--- Korea/Startup.cs
+++ Korea/Startup.cs
@@ -1,14 +1,18 @@
 using FluentValidation;
 using Handlers;
+using Handlers.Services;
 using Korea.Pipelines;
 using MediatR;
 using MediatR.Pipeline;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using System.Text;
 
 namespace Korea
 {
@@ -31,7 +35,24 @@ namespace Korea
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtSecurityKey"]))
+                    };
+                });
+
             //Register FluentValidation
             services.AddValidatorsFromAssemblies(new[] { domain });
 
